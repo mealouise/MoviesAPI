0ef3ce8644fde4af37d50f2f2cf6d91324f11fc6
mealouise <esheridan45@gmail.com>
2020-06-30 15:48:07 +0100

    initial set up of project

---
+++ MoviesAPI/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace MoviesAPI.Models
+{
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public string MovieName { get; set; }
+        public int AgeRating { get; set; }
+        public double Price { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+
+        public Movie(string movieName, int ageRating, double price, DateTime releaseDate, string genre)
+        {
+            MovieName = movieName;
+            AgeRating = ageRating;
+            Price = price;
+            ReleaseDate = releaseDate;
+            Genre = genre;
+        }
+    }
+}
